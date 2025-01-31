06fdd3cd28e45ea695d54af3952d995f5e3ee5a0
Gabe Kouki <gabekouki02@gmail.com>
2025-01-30 21:42:52 -0500

    another rapper chain, I think its 900k value now

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -11,54 +11,58 @@ import UserService from "../services/user-service";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function LandingPage() {
-  const { loginWithRedirect, isAuthenticated, isLoading, user } = useAuth0();
-  const { getUserByEmail } = UserService();
-  const [loggedInUser, setLoggedInUser] = useState<any>(null);
-  const [showAccountTypeButtons, setShowAccountTypeButtons] = useState(false);
+	const { loginWithRedirect, isAuthenticated, isLoading, user } = useAuth0();
+	const { getUserByEmail } = UserService();
+	const [loggedInUser, setLoggedInUser] = useState<any>(null);
+	const [showAccountTypeButtons, setShowAccountTypeButtons] = useState(false);
 
-  useEffect(() => {
-    const checkUserAccount = async () => {
-      if (user?.email) {
-        try {
-          const userLoggedIn = await getUserByEmail(user.email);
-          if (userLoggedIn) {
-            setLoggedInUser(userLoggedIn);
-          }
-        } catch (error: any) {
-          if (error.message === "Error: Not Found") {
-            setShowAccountTypeButtons(true);
-          }
-          console.error("Error fetching user:", error);
-        }
-      }
-    };
-    checkUserAccount();
-  }, [user]);
+	useEffect(() => {
+		const checkUserAccount = async () => {
+			if (user?.email) {
+				try {
+					const userLoggedIn = await getUserByEmail(user.email);
+					if (userLoggedIn) {
+						setLoggedInUser(userLoggedIn);
+					}
+				} catch (error: any) {
+					if (error.message === "Error: Not Found") {
+						setShowAccountTypeButtons(true);
+					}
+					console.error("Error fetching user:", error);
+				}
+			}
+		};
+		checkUserAccount();
+	}, [user]);
 
-  return (
-    <div>
-      {isLoading && !loggedInUser && (
-        <div className="spinner-container">
-          <div className="spinner"></div>
-        </div>
-      )}
-      {!isAuthenticated && !isLoading && (
-        <div className="login-button-container">
-          <button className="login-button" onClick={() => loginWithRedirect()}>
-            Log In
-          </button>
-        </div>
-      )}
+	return (
+		<div>
+			{isLoading && !loggedInUser && (
+				<div className="spinner-container">
+					<div className="spinner " />
+				</div>
+			)}
+			{!isAuthenticated && !isLoading && (
+				<div className="login-button-container">
+					<button
+						type="button"
+						className="global-button"
+						onClick={() => loginWithRedirect()}
+					>
+						Log In
+					</button>
+				</div>
+			)}
 
-      {loggedInUser && !isLoading && loggedInUser.userType === 1 && (
-        <UserSearch user={loggedInUser} />
-      )}
+			{loggedInUser && !isLoading && loggedInUser.userType === 1 && (
+				<UserSearch user={loggedInUser} />
+			)}
 
-      {loggedInUser && !isLoading && loggedInUser.userType === 2 && (
-        <ProfilePage user={loggedInUser} />
-      )}
+			{loggedInUser && !isLoading && loggedInUser.userType === 2 && (
+				<ProfilePage user={loggedInUser} />
+			)}
 
-      {showAccountTypeButtons && <AccountTypes />}
-    </div>
-  );
+			{showAccountTypeButtons && <AccountTypes />}
+		</div>
+	);
 }
